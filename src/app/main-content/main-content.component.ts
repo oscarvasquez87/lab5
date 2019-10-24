0d8f63dd6d8282855953cd9bccacf472055cada4
oscarvasquez87 <oscarvasquez87@mail.fresnostate.edu>
2019-10-24 03:51:16 -0700

    adding dat two code, finishing lab#4

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,29 +1,75 @@
 import { Component, OnInit } from '@angular/core';
 
+interface IStudent {
+  ID: number;
+  firstName: string;
+  lastName: string;
+  course: string;
+  editMode: boolean;
+}
 @Component({
   selector: 'app-main-content',
   templateUrl: './main-content.component.html',
   styleUrls: ['./main-content.component.css']
 })
 export class MainContentComponent implements OnInit {
-  greeting2 = '';
-  saying1 = 'Mow Mow';
-  saying2 = 'Num Num';
 
+  studentArray: Array<IStudent> = [];
+  disableAddButton = false;
   constructor() { }
 
   ngOnInit() {
-
+    this.studentArray = [
+      {
+        ID: 1,
+        firstName: 'Oscar',
+        lastName: 'Vasquez',
+        course: 'Programing',
+        editMode: false
+      },
+      {
+        ID: 2,
+        firstName: 'Kevin',
+        lastName: 'McDonold',
+        course: 'CIA 101',
+        editMode: false
+      },
+      {
+        ID: 3,
+        firstName: 'Steve',
+        lastName: 'Jobs',
+        course: 'Web Programing',
+        editMode: false
+      }
+    ];
   }
 
-    showGreeting2() {
-      // const row3 = document.getElementById('r2c2');
-      this.greeting2 = this.saying1 + ' ' + this.saying2 + '.';
-    }
-
-    resetGreeting2() {
-      // const row3 = document.getElementById('r2c2');
-      this.greeting2 = 'r2c2';
-
-    }
+  addStudent() {
+    this.studentArray.unshift({
+      ID: null,
+      firstName: null,
+      lastName: null,
+      course: null,
+      editMode: true
+    });
+    this.disableAddButton = true;
   }
+
+  removeStudent(index: number) {
+    this.studentArray.splice(index, 1);
+  }
+
+  saveStudent() {
+    this.studentArray[0].editMode = false;
+    this.disableAddButton = false;
+
+    this.sort('asc');
+  }
+
+  sort(direction: string) {
+    this.studentArray.sort((a: IStudent, b: IStudent) => {
+      return a.ID < b.ID ? -1 : 1;
+    });
+  }
+
+}
